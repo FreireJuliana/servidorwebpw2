e3aa04988288242b8519960db5c1b0545e8c3238
FreireJuliana <eufreire7@gmail.com>
2025-10-08 20:18:17 -0300

    aula08/10

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
+import {filmes} from "../data/filmes.js"
 function Home () {
+    console.log(filmes)
 
+ const primeiroFilme = filmes[0]
+const estiloImagem ={width: "300px"};
 return <>
 <h1>filmes </h1>
 
-<p> listagem de filmes aqui </p>;
+<ul>
+    
+    <li>
+
+ <p>nome do filme:</p>
+
+ <p> titulo: {primeiroFilme.titulo }</p>
+ <p>ano:{primeiroFilme.ano}</p>
+ <p>genero:{primeiroFilme.genero}</p>
+ <p>nota{primeiroFilme.nota}</p>
+
+ <img style={estiloImagem} src ={primeiroFilme.poster}/>
+
+    </li>
+    
+</ul>
+
+
 </>
 }
 
