c9b254561c0fd9f158de0dd3d0e957deb53f72c3
FreireJuliana <eufreire7@gmail.com>
2025-10-20 22:01:20 -0300

    Aula do dia 20/10/2025

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,40 @@
 import {filmes} from "../data/filmes.js"
+function CardFilme({filme}){
+    const estiloImagem ={width: "300px"};
+    return(
+        <li>   
+
+        <p> titulo: {filme.titulo }</p>
+        <p>ano:{filme.ano}</p>
+        <p>genero:{filme.genero}</p>
+        <p>nota:{filme.nota}</p>
+
+        <img style={estiloImagem} src ={filme.poster}/>
+        </li>
+    );
+}
+
+
+
+
 function Home () {
    
-const estiloImagem ={width: "300px"};
+
 return <>
 <h1>filmes </h1>
 
 <ul>
     { 
     filmes.map((filme)=>{
-        return(
-            <li key={`card-filme-${filme.id} `}>   
-
-                <p>nome do filme:</p>
-
-                <p> titulo: {filme.titulo }</p>
-                <p>ano:{filme.ano}</p>
-                <p>genero:{filme.genero}</p>
-                <p>nota{filme.nota}</p>
-
-                <img style={estiloImagem} src ={filme.poster}/>
-
-            </li>
-    
+        return(     
+     <CardFilme  key={`card-filme-${filme.id} `}filme={filme}/>
         );
     })}
     
 </ul>
 
-
 </>
+
 }
 
 export default Home;
